216f2a7c78d3d5d991a95d1b9cef02f08728a73e
Tristan Bagot <tristan.bagot@gmail.com>
2018-10-23 17:14:43 +0200

    init

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!class_exists('Tristantbg\VimeoFetcher')) {
+    require_once __DIR__ . '/lib/vimeofetcher.php';
+}
+
+Kirby::plugin('tristantbg/kirby3-vimeofetcher', [
+    'blueprints' => [
+        'files/vimeo' => __DIR__ . 'lib/blueprints/vimeo.yml',
+    ],
+    'hooks' => require_once __DIR__ . '/lib/hooks.php',
+]
+]);
