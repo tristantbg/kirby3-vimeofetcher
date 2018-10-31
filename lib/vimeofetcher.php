52ecbb92eebbd2045ea279aaa6e425be8935c392
Tristan Bagot <tristan.bagot@gmail.com>
2018-10-31 18:55:01 +0100

    save

--- lib/vimeofetcher.php
+++ lib/vimeofetcher.php
@@ -85,7 +85,7 @@ class VimeoFetcher
                 $extension = pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION);
                 $filename  = pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_FILENAME);
                 $imagedata = file_get_contents($url);
-                $safename  = \Kirby\Toolkit\F::safeName($body['name'] . '-' . $filename);
+                $safename  = \Kirby\Toolkit\F::safeName('vimeo_' . $body['name'] . '_' . $filename);
 
                 if ($file = $page->image($safename . '.' . $extension)) {
                     $file->update(array(
