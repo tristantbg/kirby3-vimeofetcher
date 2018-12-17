2011281fc861d2e3ffa7467d5928815eb78dd0cd
Tristan Bagot <tristan.bagot@gmail.com>
2018-12-17 11:09:26 +0100

    save

--- config.php
+++ config.php
@@ -9,4 +9,15 @@ Kirby::plugin('tristantbg/kirby3-vimeofetcher', [
         'files/vimeo' => __DIR__ . 'lib/blueprints/vimeo.yml',
     ],
     'hooks' => require_once __DIR__ . '/lib/hooks.php',
+    'fileMethods' => [
+      'vimeoSD' => function(){
+        return $this->vimeofiles()->toStructure()->filterBy('quality', 'sd');
+      },
+      'vimeoHD' => function(){
+        return $this->vimeofiles()->toStructure()->filterBy('quality', 'hd');
+      },
+      'vimeoHls' => function(){
+        return $this->vimeofiles()->toStructure()->filterBy('quality', 'hls');
+      }
+    ]
 ]);
